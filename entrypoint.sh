552b5cda8cf5a1c6cd2538b71e3842c64c6352ae
Blocksmith <contact@blocksmith.net>
2021-05-06 13:36:56 +0200

    Update entrypoint.sh
    create tag version then copy files on it

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ fi
 OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
 
 CLONE_DIR=$(mktemp -d)
+TAG_VERSION=$(git describe --tags)
 
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
@@ -23,8 +24,8 @@ git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/$TAG_VERSION
+cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$TAG_VERSION"
 cd "$CLONE_DIR"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
